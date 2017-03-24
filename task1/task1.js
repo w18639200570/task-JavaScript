84e3721ea5b7c647673f39e6837b34ab1f55c404
w18639200570 <710125233@qq>
2017-03-24 21:47:05 +0800

    加入文件

---
+++ task1/task1.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Shinelon on 2017/3/24.
+ */
+function one(onecell) {
+    cell = getElementTagName("div");
+    var cell = Math.floor((Math.random()*cell.length+3));
+    setAttribute("background","blue");
+    alert(cell[index]);
+}
