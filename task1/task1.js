d01b3b084341d785f57c61b9a101bb70e98baa73
w18639200570 <710125233@qq>
2017-03-26 11:31:29 +0800

    js

--- task1/task1.js
+++ task1/task1.js
@@ -2,14 +2,21 @@
  * Created by Shinelon on 2017/3/24.
  */
 function one(onecell) {
-    var cell = document.getElementById("placeholder");
+    var cell = document.getElementsByTagName("div"[1]);
+
+
+
     // var ran = Math.floor(Math.random()*cell.length);
-    var sex = onecell.getAttribute("red");
+    // var sex = onecell.getAttribute("red");
     // colors[0] = "blue";
     // colors[1] = "red";
     // colors[2] = "green";
     // colors[3] = "black";
-    cell.setAttribute("style",sex);
+    // cell.setAttribute("style",sex);
 }
 
+function setbgc() {
+	var hong = document.getElementById("red");
+	hong.setAttribute("style","blue");
+}
 
