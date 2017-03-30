38f6391fc35b65abc78ebd3f86e8638238232697
w18639200570 <710125233@qq>
2017-03-30 14:49:09 +0800

    加入文件

--- task1/2.js
+++ task1/2.js
@@ -19,29 +19,29 @@ function colors(){
 // 设置颜色主要内容
 function setbgc(){
     // 设置三个随机数字
-    var num = parseInt(Math.random()*9);
-    var numo = parseInt(Math.random()*9);
-    var numt = parseInt(Math.random()*9);
+    var num = parseInt(Math.random()*8);
+    var numo = parseInt(Math.random()*8);
+    var numt = parseInt(Math.random()*8);
 
 // 防止变成橙色
     if (colors()=="#ffa500"){
         colors();
     }
     // 处理数字相同问题
-    // if (num===numo||num===numt) {num++;}
-    // if (numo===num||numo===numt){numo++;}
-    // if (numt===num||numt===numo){numt++;}
-    // if(num >= 9){num--;}
-    // if(numo >= 9){num--;}
-    // if(numt >= 9){num--;}
+    if (num===numo||num===numt) {num++;}
+    if (numo===num||numo===numt){numo++;}
+    if (numt===num||numt===numo){numt++;}
+    if(num >= 8){num--;}
+    if(numo >= 8){num--;}
+    if(numt >= 8){num--;}
     // 另一种处理数字问题
-    function random() {
-        return  Math.floor(Math.random()*9)
-    }
+    // function random() {
+    //     return  Math.floor(Math.random()*9)
+    // }
     // 给随机格子随机颜色
-    co[random()].style.backgroundColor=colors();
-    co[random()].style.backgroundColor=colors();
-    co[random()].style.backgroundColor=colors();
+    co[num].style.backgroundColor=colors();
+    co[numo].style.backgroundColor=colors();
+    co[numt].style.backgroundColor=colors();
     // 写出数据查找报错用
     console.log(co[num].style.backgroundColor);
     console.log(co[numo].style.backgroundColor);
@@ -55,18 +55,39 @@ function setbgc(){
 // 设置循环定时器
 var int;
 function start(){
-    int = window.setInterval(function(){
-        end();
-        setbgc();
-    },1000);
-}
+        int = window.setInterval(function(){
+            end();
+            setbgc();
+        },1000);
+    star.disabled=true;
+    }
+    //设置单次定时器(失败）
+//     int = window.setTimeout(function(){
+//         end();
+//         setbgc();
+//     }, 1000);
+//     window.setTimeout(function(){
+//         end();
+//         setbgc();
+//     },1000)
+// }
 // 复位用
 function end(){
     for(var i=0;i < co.length;i++){
     co[i].style.backgroundColor="orange";
     }
+    // 新方法(失败，只能选中一个，上边方法可以选中每一个 )
+    // var i=co.length-1;
+    // co[i].style.backgroundColor="orange";
 }
 // 本来是要用来清除定时器，但是好像在按键上设置clearInterval先实现了，暂时不需要了
 // function en(){
 //     clearInterval(int);
-// }
+//     for(var i=0;i < co.length;i++){
+//         co[i].style.backgroundColor="orange";}
+// }}
+// 修改定时器会多次生效问题
+var star = document.getElementById("star");
+function over(){
+    star.disabled=false;
+}
